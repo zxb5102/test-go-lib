b708dbd4b54b187d5cc56ad6df6226ac59ead6c6
zhongxb <1156594221@qq.com>
2023-06-06 22:59:40 +0800

    dd.cc.ee/v2

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/zxb5102/test-go-lib/v2
+module dd.cc.ee/v2
 
 go 1.20
--- utils/tool.go
+++ utils/tool.go
@@ -1,5 +1,5 @@
 package utils
 
 func GetMsg() string {
-	return "get_msg_v2.1.1"
+	return "get msg dd.cc.ee/v2"
 }
